a301363a5bf900e6040dcefb427acd78d8d994ac
Pi1at <anatol.sv@gmail.com>
2016-06-17 04:37:45 +0300

    23 solved

---
+++ src/CodeEval/Easy/23-MultiplicationTables.fsx
@@ -0,0 +1,23 @@
+﻿// OUTPUT SAMPLE
+//1   2   3   4   5   6   7   8   9  10  11  12
+//2   4   6   8  10  12  14  16  18  20  22  24
+//3   6   9  12  15  18  21  24  27  30  33  36
+module ``23-MultiplicationTables``
+
+open System
+open System.IO
+
+let solve() = 
+    for x in 1..12 do
+        for y in 1..12 do
+            printf "%4i" (x * y)
+        printfn ""
+    0
+#if INTERACTIVE
+
+solve()
+#else
+[<EntryPoint>]
+let main (args) =
+    solve ()
+#endif
